0d451761524accdcb2a735d772d843f08d017901
Aneurin Toomey <ceanncliste@gmail.com>
2024-03-16 20:13:02 -0600

    Updated Live Telemetry for new config

--- Live_Telemetry/src/mflivetelemetry.py
+++ Live_Telemetry/src/mflivetelemetry.py
@@ -27,20 +27,21 @@ def cli(portname) -> None:
     write_api = client.write_api(write_options=SYNCHRONOUS)
 
     while(True):
-        point: list[str] = str(ser.readline().decode()).split(",")
+        point: list[str] = str(ser.readline().decode()).replace('\n', '').replace('\r', '').split(",")
+        print(point)
 
-        if (point[0] == "1050"):
+        if (point[0] == "0"):
             names = []
             for i in range(2,len(point)):
                 names.append(point[i])
 
-            posNames.update({point[1], names})
+            posNames.update({point[1]: names})
 
-        elif (point[0] == "1051"):
-            for i in range(0,len(posNames[point[0]])):
-                names.append(point[i])
-                p = Point(point[1]).tag("Data_Source", point[1]).field(posNames[point[1]][i], float(point[i+2]))
-                write_api.write(bucket="daqlive", record=p, org="minesformula")
+        elif (point[0] == "1" and posNames and len(point) == 11):
+            if (point[1] in posNames.keys()):
+                for i in range(0,len(posNames[point[1]])):
+                    p = Point(str(point[1])).tag("SensorNum", str(point[2])).field(posNames[point[1]][i], float(point[i+3]))
+                    write_api.write(bucket="daqlive", record=p, org="minesformula")
 
 if (__name__ == "__main__"):
     cli()
